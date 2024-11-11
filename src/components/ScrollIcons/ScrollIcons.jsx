9d70b2944b4ced77cb5f395756dab1e7e532ffd0
BobchopGaming <jmomolon@gmail.com>
2024-11-11 23:41:13 +0100

    push

--- src/components/ScrollIcons/ScrollIcons.jsx
+++ src/components/ScrollIcons/ScrollIcons.jsx
@@ -29,18 +29,22 @@ const ScrollIcons = () => {
       {showMailIcon && (
         <div
           onClick={() => setModalOpen(true)}
-          className="fixed bottom-32 right-6 bg-lime-500 p-4 rounded-full cursor-pointer shadow-lg transition-transform transform hover:scale-110 z-50"
+          className="fixed bottom-28 left-10 bg-lime-500 p-4 rounded-full cursor-pointer shadow-lg transition-transform transform hover:scale-110 z-50"
         >
           <RiMessage2Line className="text-white text-2xl" />
         </div>
       )}
       {showScrollUpIcon && (
-        <div
-          onClick={scrollToTop}
-          className="fixed bottom-10 left-10 bg-blue-500 p-4 rounded-full cursor-pointer shadow-lg transition-transform transform hover:scale-110 z-50"
-        >
-          <FaHandPointUp className="text-white text-2xl" />
-        </div>
+      <div
+      onClick={scrollToTop}
+      className="group fixed bottom-10 left-10 bg-blue-500 p-4 rounded-full cursor-pointer shadow-lg transition-transform transform hover:scale-110 z-50"
+    >
+      <FaHandPointUp className="text-white text-2xl" />
+      <span className="absolute bottom-12 right-0 transform translate-x-1/2 opacity-0 group-hover:opacity-100 text-white text-xs transition-opacity bg-blue-700 px-3 py-1 rounded-lg shadow-lg">
+    Back to Top
+  </span>
+    </div>
+    
       )}
       <ContactModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
     </>
